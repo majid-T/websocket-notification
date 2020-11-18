8e5e1eb732f1a859e197a6b580313ea4f861ba98
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-11-17 21:16:43 -0500

    Added server skeleton

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express();
+const WS_PORT = 4000;
+// Web Socket Server
+const wsServer = app.listen(WS_PORT, () => {
+    console.log(`Web socket server is running on port ${WS_PORT}`)
+})
