7104ab10f6dccc12903ebabda7e86510d9b62f58
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-11-17 21:46:58 -0500

    Added socket connection'

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 const WS_PORT = 4000;
@@ -8,4 +9,12 @@ const wsServer = app.listen(WS_PORT, () => {
 })
 
 // Home Page serving file
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
+
+
+// Web Socket configuration
+const io = socket(wsServer);
+
+io.on('connection', (clientSocket) => {
+    console.log(`New connection stablished... ${clientSocket.id}`)
+})
---
+++ public/notification.js
@@ -0,0 +1,5 @@
+//WS URL
+const WS_SERVER = 'http://localhost:4000';
+
+//Connect to wsServer
+const socket = io.connect(WS_SERVER)
